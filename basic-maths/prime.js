02d8eddd805a09314cd31ae62a74800a0d422a99
ishan09ahmad <mdishanahmad@gmail.com>
2025-12-23 22:29:43 +0530

    basic math questions

--- basic-maths/prime.js
+++ basic-maths/prime.js
@@ -1,21 +1,18 @@
 // Check if a number is prime or not
 
-let n=19;
-let count=0;
-for (let i = 2; i*i <=n; i++) {
- if(n%i==0)
- {
-  count++;
-  break;
- }
-
-}
-if(count==0)
-{
-  console.log("prime")
-}
-else
-{
-  console.log("not a prime")
+let n = 19;
+let count = 0;
+for (let i = 1; i * i <= n; i++) {
+  if (n % i == 0) {
+    count++;
+    if (n / i !== i) {
+      count++;
+    }
+  }
 }
 
+if (count == 2) {
+  console.log("prime");
+} else {
+  console.log("not a prime");
+}
