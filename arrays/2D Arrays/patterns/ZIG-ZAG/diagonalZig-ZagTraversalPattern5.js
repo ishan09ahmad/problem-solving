46f6bb8c9d37ea1890aed5ca908386967494dae8
ishan09ahmad <mdishanahmad@gmail.com>
2026-02-12 16:13:59 +0530

    2d arrays zig-zag pattern

---
+++ arrays/2D Arrays/patterns/ZIG-ZAG/diagonalZig-ZagTraversalPattern5.js
@@ -0,0 +1,37 @@
+// pattern 5 with (n-1,0)
+
+let arr = [
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9, 10, 11, 12],
+  [13, 14, 15, 16],
+];
+
+let n = arr.length;
+let m = arr[0].length;
+
+let i = n - 1;
+let j = 0;
+for (let k = 0; k < n * m; k++) {
+  console.log(arr[i][j]);
+
+  if ((i + j) % 2 == 0) {
+    if (j == m - 1) {
+      i--;
+    } else if (i == n - 1) {
+      j++;
+    } else {
+      i++;
+      j++;
+    }
+  } else {
+    if (i == 0) {
+      j++;
+    } else if (j == 0) {
+      i--;
+    } else {
+      i--;
+      j--;
+    }
+  }
+}
---
+++ arrays/2D Arrays/patterns/ZIG-ZAG/diagonalZig-ZagTraversalPattern7.js
@@ -0,0 +1,40 @@
+//pattern 7 with (n-1,m-1)
+
+
+let arr = [
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9, 10, 11, 12],
+  [13, 14, 15, 16],
+];
+
+let n = arr.length;
+let m = arr[0].length;
+
+let i = n - 1;
+let j = m - 1;
+for (let k = 0; k < n * m; k++) {
+  console.log(arr[i][j]);
+
+  if ((i + j) % 2 == 0) {
+    if (j == m - 1) {
+      i--;
+    } else if (i == 0) {
+      j--;
+    } else {
+      i--;
+      j++;
+    }
+  } else {
+
+    if (j == 0) {
+      i--;
+    }
+    else  if (i == n - 1) {
+      j--;
+    }  else {
+      i++;
+      j--;
+    }
+  }
+}
